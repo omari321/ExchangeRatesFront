c465078bb82c212db7d2e1fc75ca6ba5bdd53fda
omo <omo@DESKTOP-MK2IP3Q>
2023-06-14 22:32:44 +0400

    add second page data

---
+++ src/app/modules/landing/historicExchangeRates/historic-exchangeRates.component.ts
@@ -0,0 +1,94 @@
+import {Component, OnInit, ViewEncapsulation} from '@angular/core';
+import {FormControl, ReactiveFormsModule} from '@angular/forms';
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatSelectModule} from "@angular/material/select";
+import {MatTableModule} from "@angular/material/table";
+import {CommonModule} from '@angular/common';
+import { DateTime } from 'luxon';
+import { HistoricExchangeRateService } from './historic-exchangeRates.service';
+import { BehaviorSubject } from 'rxjs';
+
+@Component({
+    selector: 'historic-exchange-rates',
+    standalone: true,
+    templateUrl: './historic-exchangeRates.component.html',
+    imports: [
+        MatFormFieldModule,
+        MatSelectModule,
+        MatTableModule,
+        ReactiveFormsModule,
+        CommonModule
+    ]
+})
+export class HistoricExchangeRatesComponent implements OnInit {
+    data=[]
+    selectedCurrency: string = 'USD';
+    selectControl = new FormControl<string>('USD');
+    selectDaysPeriod= new FormControl<number>(7);
+    selectedBank= new FormControl<string>(null);
+    availableBanks= new BehaviorSubject<string[]>(null);
+    constructor(private historicExchangeRatesService: HistoricExchangeRateService) {
+    }
+
+    ngOnInit(): void {
+
+        this.getHistoricData();
+
+        this.selectControl.valueChanges.
+            subscribe(x=>{
+                this.selectedCurrency=x;
+                this.getHistoricData();
+            })
+        this.selectDaysPeriod.valueChanges
+        .subscribe(x=>
+        {
+            this.getHistoricData();
+        });
+    }
+
+    getHistoricData(): void{
+        this.historicExchangeRatesService
+            .getHistoricExchangeRates(this.selectedCurrency,this.selectDaysPeriod.value)
+            .subscribe(x=>
+                {
+                    this.availableBanks.next(x.data[0].bankRates.map((value)=>
+                    {
+                       return value.bankName
+                    }));
+                })
+    }
+
+    mapCurrencyCodeToName(selectedCurrency: string): string {
+        switch (selectedCurrency) {
+            case 'USD':
+                return 'United States Dollar';
+            case 'EUR':
+                return 'Euro';
+            case 'GBP':
+                return 'British Pound';
+            case 'CHF':
+                return 'Swiss Franc';
+            case 'RUB':
+                return 'Russian Ruble';
+            case 'TRY':
+                return 'Turkish Lira';
+        }
+    }
+
+    mapCurrencyCodeToFlagURL(selectedCurrency: string): string {
+        switch (selectedCurrency) {
+            case 'USD':
+                return "https://flagsapi.com/US/flat/32.png";
+            case 'EUR':
+                return "https://flagcdn.com/w40/eu.png"
+            case 'GBP':
+                return "https://flagsapi.com/GB/flat/32.png";
+            case 'CHF':
+                return "https://flagsapi.com/CH/flat/32.png";
+            case 'RUB':
+                return "https://flagsapi.com/RU/flat/32.png";
+            case 'TRY':
+                return "https://flagsapi.com/TR/flat/32.png";
+        }
+    }
+}
