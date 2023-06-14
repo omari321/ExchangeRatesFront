8939f1d7daf7478b932de69d3f47918102672806
omo <omo@DESKTOP-MK2IP3Q>
2023-06-14 17:54:49 +0400

    mc

--- src/app/mock-api/common/navigation/data.ts
+++ src/app/mock-api/common/navigation/data.ts
@@ -2,12 +2,20 @@
 import { FuseNavigationItem } from '@fuse/components/navigation';
 
 export const defaultNavigation: FuseNavigationItem[] = [
+    //onse used
     {
         id   : 'exchange-rates',
         title: 'Exchange Rates',
         type : 'basic',
         icon : 'heroicons_outline:chart-pie',
         link : '/exchange-rates'
+    },
+    {
+        id   : 'historical-exchange-rates',
+        title: 'Historical Exchange Rates',
+        type : 'basic',
+        icon : 'heroicons_outline:calendar',
+        link : '/historical-exchange-rates'
     }
 ];
 export const compactNavigation: FuseNavigationItem[] = [
