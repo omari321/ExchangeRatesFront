646aeb5fd121b26a9f4bdabe41072323f62b067a
vajbon3 <=>
2023-06-15 10:00:57 +0400

    split buy and sell data into selection

--- src/app/modules/landing/historicExchangeRates/historic-exchangeRates.component.ts
+++ src/app/modules/landing/historicExchangeRates/historic-exchangeRates.component.ts
@@ -4,12 +4,12 @@ import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatSelectModule} from "@angular/material/select";
 import {MatTableModule} from "@angular/material/table";
 import {CommonModule} from '@angular/common';
-import { DateTime } from 'luxon';
-import { HistoricExchangeRateService } from './historic-exchangeRates.service';
-import { BehaviorSubject, Subscription } from 'rxjs';
-import { NgChartsModule } from 'ng2-charts';
-import { ChartConfiguration, ChartOptions } from 'chart.js';
-import { BankCurrencyInformationDto } from './historic-exchangeRates.models';
+import {DateTime} from 'luxon';
+import {HistoricExchangeRateService} from './historic-exchangeRates.service';
+import {BehaviorSubject, Subscription} from 'rxjs';
+import {NgChartsModule} from 'ng2-charts';
+import {ChartConfiguration, ChartOptions} from 'chart.js';
+import {BankCurrencyInformationDto} from './historic-exchangeRates.models';
 
 @Component({
     selector: 'historic-exchange-rates',
@@ -24,44 +24,115 @@ import { BankCurrencyInformationDto } from './historic-exchangeRates.models';
         NgChartsModule
     ]
 })
-export class HistoricExchangeRatesComponent implements OnInit , OnDestroy {
+export class HistoricExchangeRatesComponent implements OnInit, OnDestroy {
     selectedCurrency: string = 'USD';
     selectControl = new FormControl<string>('USD');
-    selectDaysPeriod= new FormControl<number>(7);
-    selectedBank= new FormControl<string>(null);
-    availableBanks= new BehaviorSubject<string[]>(null);
+    selectDaysPeriod = new FormControl<number>(7);
+    selectedBank = new FormControl<string>(null);
+    availableBanks = new BehaviorSubject<string[]>(null);
+    selectedExchange = new FormControl<string>('buy');
 
     title = 'ng2-charts-demo';
 
     lineChartData?: ChartConfiguration<'line'>['data'];
     public lineChartOptions: ChartOptions<'line'> = {
-        responsive: false
-      };
+        responsive: true,
+    };
     public lineChartLegend = true;
 
     private _data: BankCurrencyInformationDto[];
-    private _subscriptions: Subscription[]=[];
+    private _subscriptions: Subscription[] = [];
+    availableExchanges = new BehaviorSubject<Object>({buy: {}, sell: {}});
+
     constructor(private historicExchangeRatesService: HistoricExchangeRateService) {
     }
 
 
-    getHistoricData(): void{
+    getHistoricData(selectedBankCaller:boolean = false): void {
         this._subscriptions.push(
             this.historicExchangeRatesService
-                .getHistoricExchangeRates(this.selectedCurrency,this.selectDaysPeriod.value)
-                .subscribe(x=>
-                    {
-                        console.log(x);
-
-                        this._data=x.data;
+                .getHistoricExchangeRates(this.selectedCurrency, this.selectDaysPeriod.value)
+                .subscribe(x => {
+                    this._data = x.data;
+                    if(!selectedBankCaller){
                         this.selectedBank.setValue(x.data[0].bankRates[0].bankName);
-                        this.availableBanks.next(x.data[0].bankRates.map((value)=>
-                        {
+                    }
+                    this.availableBanks.next(x.data[0].bankRates.map((value) => {
                         return value.bankName
-                        }));
-                    })
+                    }));
+                    this.updateChart();
+                })
         );
     }
+
+    updateChart(): void {
+        console.log('chart updated');
+        this.availableExchanges = new BehaviorSubject<Object>({
+            buy: {
+                data: this._data.map(x => x.bankRates.filter(y => y.bankName == this.selectedBank.value)[0].buyRate).reverse(),
+                label: 'Buy Rate',
+                fill: false,
+                tension: 0.5,
+                borderColor: 'rgba(255,0,0,0.3)',
+                backgroundColor: 'black',
+                borderWidth: 1,
+            },
+            sell: {
+                data: this._data.map(x => x.bankRates.filter(y => y.bankName == this.selectedBank.value)[0].sellRate).reverse(),
+                label: 'Sell Rate',
+                fill: false,
+                tension: 0.5,
+                borderColor: 'rgba(50, 166, 168,0.3)',
+                backgroundColor: 'black',
+                borderWidth: 1,
+            }
+        });
+        this.lineChartData = {
+            labels: this._data.map(x => x.date).reverse(),
+            datasets: [
+                this.availableExchanges.value[this.selectedExchange.value]
+            ],
+        };
+    }
+
+    ngOnInit(): void {
+
+        // fetch initially
+        this.getHistoricData();
+
+        // fetch based on selection
+        this._subscriptions.push(
+            this.selectControl.valueChanges.subscribe(x => {
+                this.selectedCurrency = x;
+                this.getHistoricData();
+            })
+        );
+        this._subscriptions.push(
+            this.selectDaysPeriod.valueChanges.subscribe(x => {
+                this.getHistoricData();
+            })
+        );
+
+        this._subscriptions.push(
+            this.selectedBank.valueChanges.subscribe(x => {
+                    this.getHistoricData(true);
+                }
+            )
+        );
+
+        this._subscriptions.push(
+            this.selectedExchange.valueChanges.subscribe(x => {
+                    this.getHistoricData();
+                })
+        );
+    }
+
+    ngOnDestroy()
+        :
+        void {
+        this._subscriptions.forEach(x => x.unsubscribe());
+    }
+
 
     mapCurrencyCodeToName(selectedCurrency: string): string {
         switch (selectedCurrency) {
@@ -96,53 +167,4 @@ export class HistoricExchangeRatesComponent implements OnInit , OnDestroy {
                 return "https://flagsapi.com/TR/flat/32.png";
         }
     }
-    ngOnInit(): void {
-
-        this.getHistoricData();
-        this._subscriptions.push(
-            this.selectControl.valueChanges.subscribe(x=>{
-                    this.selectedCurrency=x;
-                    this.getHistoricData();
-
-            })
-        );
-        this._subscriptions.push(
-            this.selectDaysPeriod.valueChanges.subscribe(x=>
-            {
-                this.getHistoricData();
-            })
-        );
-        this._subscriptions.push(
-            this.selectedBank.valueChanges.subscribe(x=>
-            {
-                this.lineChartData={
-                    labels: this._data.map(x=>x.date).reverse(),
-                    datasets : [
-                        {
-                            data: this._data.map(x=>x.bankRates.filter(y=>y.bankName==this.selectedBank.value)[0].buyRate).reverse(),
-                            label: 'Buy Rate',
-                            fill: true,
-                            tension: 0.5,
-                            borderColor: 'black',
-                            backgroundColor: 'rgba(255,0,0,0.3)'
-                        },
-                        {
-                            data: this._data.map(x=>x.bankRates.filter(y=>y.bankName==this.selectedBank.value)[0].sellRate).reverse(),
-                            label: 'Sell Rate',
-                            fill: true,
-                            tension: 0.5,
-                            borderColor: 'black',
-                            backgroundColor: 'rgba(50, 166, 168,0.3)',
-
-                        }
-                    ]
-                }
-                console.log(this.lineChartData);
-            })
-        );
-    }
-    ngOnDestroy(): void {
-        this._subscriptions.forEach(x=>x.unsubscribe());
-    }
-
 }
